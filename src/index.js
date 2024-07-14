bdf646da3242a7ad238d92e1a25a3ad5ca747658
ktpark <qkrrudxkr77@naver.com>
2024-07-15 00:15:39 +0900

    Feat: Custom Hook 사용해보기

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Accommodate from './chapter_07/Accommodate';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <Accommodate />,
+  // </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
